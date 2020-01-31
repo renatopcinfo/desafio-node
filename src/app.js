62e9269a1af9c12b7e0894666cf41ce46a2fda63
renatopcinfo <renatopcinfo@gmail.com>
2020-01-31 17:01:11 -0300

    header

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ import 'dotenv/config';
 
 import express from 'express';
 import cors from 'cors';
-import Youch from 'youch';
 import routes from './routes';
 
 import './database';
@@ -10,32 +9,25 @@ import './database';
 class App {
   constructor() {
     this.server = express();
-
-    this.middlewares();
     this.routes();
-    this.exceptionHandler();
   }
 
   middlewares() {
     this.server.use(cors());
-
     this.server.use(express.json());
+
+    this.server.use((req, res, next) => {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept'
+      );
+      next();
+    });
   }
 
   routes() {
     this.server.use(routes);
-  }
-
-  exceptionHandler() {
-    this.server.use(async (err, req, res, next) => {
-      if (process.env.NODE_ENV === 'development') {
-        const errors = await new Youch(err, req).toJSON();
-
-        return res.status(500).json(errors);
-      }
-
-      return res.status(500).json({ error: 'Internal server error' });
-    });
   }
 }
 
